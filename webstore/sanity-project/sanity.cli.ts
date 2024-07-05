a3d4c67c8857956b669bd5cf955e0076d9a8949a
Adetayo <tasayu114321@gmail.com>
2024-07-05 08:46:29 +0100

    project intro

---
+++ webstore/sanity-project/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'dj9jgfug',
+    dataset: 'production'
+  }
+})
---
+++ webstore/sanity-project/schemaTypes/category.ts
@@ -0,0 +1,10 @@
+export default {
+    name: 'category',
+    type: 'document',
+    title: 'Category',
+    fields: [{
+        name: 'name',
+        title: 'name of category',
+        type: 'string',
+    }]
+}
